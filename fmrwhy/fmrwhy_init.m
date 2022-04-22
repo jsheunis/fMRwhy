d3c1f25a66c8606a06f2bca36c04be077e830b5c
Stephan Heunis <s.heunis@fz-juelich.de>
2022-04-22 14:59:21 +0200

    add option to specify dependency dir for init

--- fmrwhy/fmrwhy_init.m
+++ fmrwhy/fmrwhy_init.m
@@ -1,15 +1,19 @@
-% add fmrwhy and subdirectories to MATLAB PATH
-current_path = mfilename('fullpath');
-ind = strfind(current_path, 'fMRwhy');
-fmrwhy_dir = [current_path(1:ind + 5) filesep 'fmrwhy'];
-addpath(genpath(fmrwhy_dir));
-dependency_dir = [current_path(1:ind + 5) filesep 'dependencies'];
-addpath([dependency_dir filesep 'spm12']);
-addpath(genpath([dependency_dir filesep 'bids-matlab']));
-addpath(genpath([dependency_dir filesep 'dicm2nii']));
-addpath(genpath([dependency_dir filesep 'tapas' filesep 'physio' filesep 'code']));
-tapas_physio_init()
-addpath(genpath([dependency_dir filesep 'RainCloudPlots']));
+function fmrwhy_init(dependency_dir)
+    % add fmrwhy and subdirectories to MATLAB PATH
+    current_path = mfilename('fullpath');
+    ind = strfind(current_path, 'fMRwhy');
+    fmrwhy_dir = [current_path(1:ind + 5) filesep 'fmrwhy'];
+    addpath(genpath(fmrwhy_dir));
 
-% check if dependencies are installed and throw warnings if not
-fmrwhy_util_checkDependencies([]);
+    if nargin<1
+        dependency_dir = [current_path(1:ind + 5) filesep 'dependencies'];
+
+    addpath([dependency_dir filesep 'spm12']);
+    addpath(genpath([dependency_dir filesep 'bids-matlab']));
+    addpath(genpath([dependency_dir filesep 'dicm2nii']));
+    addpath(genpath([dependency_dir filesep 'tapas' filesep 'physio' filesep 'code']));
+    tapas_physio_init()
+    addpath(genpath([dependency_dir filesep 'RainCloudPlots']));
+
+    % check if dependencies are installed and throw warnings if not
+    fmrwhy_util_checkDependencies([]);
