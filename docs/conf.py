41b5498528943282755a9e42cc3964ae25946290
Remi Gau <remi_gau@hotmail.com>
2022-05-17 21:55:22 +0200

    fix some warnings and dead code in docs

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # installing theme package
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -76,4 +75,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
